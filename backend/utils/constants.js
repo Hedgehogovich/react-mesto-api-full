50730e03890861fe13bced7101a03c4b70390254
Yuriy Chachilo <chajurassic@icloud.com>
2021-05-10 16:59:06 +0300

    fix: использование запасных переменных

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -2,8 +2,8 @@ const BCRYPT_SALT_ROUNDS = 10;
 const MONGODB_DUPLICATE_ERROR_CODE = 11000;
 const URL_REGEXP = /https?:\/\/(www\.)?[-a-zA-Z0-9]{1,256}\.[a-zA-Z0-9]{2,6}\b([-a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*)#?/;
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
-const FRONTEND_ORIGIN = process.env.FRONTEND_URL;
-const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
+const FRONTEND_ORIGIN = process.env.FRONTEND_URL || 'http://localhost:3001';
+const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY || 'some-secret-key';
 const JWT_SESSION_COOKIE = 'jwt';
 const SET_COOKIE_PARAMS = {
   httpOnly: true,
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,7 +1,7 @@
 const ESCAPE_BUTTON_KEY = 'Escape';
 const FORM_WHITE_THEME = 'white';
 const FORM_DARK_THEME = 'dark';
-const BASE_API_URL = process.env.REACT_APP_BACKEND_URL;
+const BASE_API_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
 
 export {
   ESCAPE_BUTTON_KEY,
